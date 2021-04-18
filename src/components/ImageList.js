ff0b151690f3b6035ee0f1b842137e6025236e81
Ahmad Enayathullah <ahmad.enayathullah@fdmgroup.com>
2021-04-19 02:07:54 +0800

    updated styling

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,16 +1,16 @@
+import './ImageList.css';
 import React from "react";
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
-  const images = props.images.map(({ description, id, urls }) => {
+  const images = props.images.map((image) => {
     //map through images array
     return (
-      <div key={id}>
-        <img src={urls.regular} alt={description} />
-      </div>
+     <ImageCard key={image.id} image={image} />
     );
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
