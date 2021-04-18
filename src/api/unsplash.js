2dc75475f38fbe16492ce1710477737b72cecfc9
enaya <therollingambit@gmail.com>
2021-04-19 01:21:26 +0800

    Delete unsplash.js

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com",
-  headers: {
-    Authorization: "Client-ID v5nauvuw_Mr1YTK17gI5166cG2sWUSQNDQRq2bXRukA",
-  },
-});
